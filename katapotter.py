8cb49cae23f744da069fc2d76653e94cd0fc2fd0
Oscar Campos Ruiz <oscar.campos@deliriumcoder.com>
2012-03-17 23:21:43 +0100

    Test Edge Two

--- katapotter.py
+++ katapotter.py
@@ -1,7 +1,7 @@
 
 # -*- test-case-name: test.katapotter.test_katapotter -*-
 # Copyright (c) 2012 - Oscar Campos <oscar.campos@member.fsf.org>
-# See LICENSE for more details 
+# See LICENSE for details 
 
 class BooksPack(object):
 	"""
@@ -12,18 +12,21 @@ class BooksPack(object):
 		self._books = books
 
 
-	def calculate(self):
+	def calculate(self):		
 		price = 0
 		discounts = [0, 1, 0.95, 0.90, 0.80, 0.75]
 		packs = []
-
+		
 		for books_number in self._books:			
 			for i in range(0, books_number):				
 				if len(packs) <= i:
 					packs.append(0)
 
-				packs[i] +=1;
-		
+				if packs[i] >= 4:
+					packs[i+1] +=1				
+				else:
+					packs[i] +=1;		
+				
 		for pack in packs:			
 			price += (8 * pack) * discounts[pack]
 
--- test/test_katapotter.py
+++ test/test_katapotter.py
@@ -1,3 +1,7 @@
+
+#Copyright (c) 2012 - Oscar Campos <oscar.campos@member.fsf.org>
+# See LICENSE for details
+
 from twisted.trial import unittest
 
 from kataPotter.katapotter import BooksPack
@@ -23,3 +27,12 @@ class BooksPackTest(unittest.TestCase):
 	def test_two_dif_one_not_books(self):
 		books = BooksPack([2, 1])
 		self.assertEqual(books.calculate(), ((8*2)*0.95)+8)
+
+	def test_edge_case_one(self):
+		books = BooksPack([2, 2, 2, 1, 1])
+		self.assertEqual(books.calculate(), 2 * (8 * 4 * 0.80))
+
+	def test_edge_case_two(self):
+		books = BooksPack([5, 5, 4, 5, 4])
+		self.assertEqual(books.calculate(), 
+			3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8))
